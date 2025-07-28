43e2cfdd6c5336d89c3292e6271f4fb1ffeb717e
Chaitanya Sai Meka <marvelchaitanya@gmail.com>
2025-07-28 11:23:16 +0530

    maded rresponsive

--- app/Xeon/home/navbar.tsx
+++ app/Xeon/home/navbar.tsx
@@ -82,7 +82,7 @@ const Navbar: React.FC<NavbarProps> = ({ searchLocation, setSearchLocation }) =>
 
       {/* Bottom Nav */}
       <div className="flex justify-center py-4">
-        <div className="border border-gray-300 rounded-full px-4 py-2 flex items-center gap-4 w-[700px] bg-white shadow-md">
+        <div className="border border-gray-300 rounded-full px-4 py-2 flex items-center gap-4 w-[90%] max-w-[400px] md:w-full md:max-w-[700px] bg-white shadow-md">
           <div className="flex-1">
             <input
               className="w-full px-2 py-1 text-sm placeholder-gray-500 focus:outline-none"
@@ -93,8 +93,8 @@ const Navbar: React.FC<NavbarProps> = ({ searchLocation, setSearchLocation }) =>
             />
             <div className="text-xs text-gray-500 ml-2">Search destinations</div>
           </div>
-          <div className="border-l border-gray-300 h-8"></div>
-          <div className="flex-1">
+          <div className="border-l border-gray-300 h-8 hidden md:block"></div>
+          <div className="flex-1 hidden md:block">
             <input
               className="w-full px-1 py-1 text-sm placeholder-gray-500 focus:outline-none"
               type="text"
@@ -109,8 +109,8 @@ const Navbar: React.FC<NavbarProps> = ({ searchLocation, setSearchLocation }) =>
             />
             <div className="text-xs text-gray-500 ml-1">Add dates</div>
           </div>
-          <div className="border-l border-gray-300 h-8"></div>
-          <div className="flex-1">
+          <div className="border-l border-gray-300 h-8 hidden md:block"></div>
+          <div className="flex-1 hidden md:block">
             <input
               className="w-full px-1 py-1 text-sm placeholder-gray-500 focus:outline-none"
               type="text"
@@ -125,8 +125,8 @@ const Navbar: React.FC<NavbarProps> = ({ searchLocation, setSearchLocation }) =>
             />
             <div className="text-xs text-gray-500 ml-1">Add dates</div>
           </div>
-          <div className="border-l border-gray-300 h-8"></div>
-          <div className="flex-1 flex items-center justify-between">
+          <div className="border-l border-gray-300 h-8 hidden md:block"></div>
+          <div className="flex-1 flex items-center justify-between hidden md:flex">
             <div>
               <input
                 className="w-full px-2 py-1 text-sm placeholder-gray-500 focus:outline-none"
@@ -154,6 +154,22 @@ const Navbar: React.FC<NavbarProps> = ({ searchLocation, setSearchLocation }) =>
               </svg>
             </button>
           </div>
+          <button className="bg-Paynes-Grey text-white rounded-full p-2 md:hidden">
+            <svg
+              className="w-5 h-5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </button>
         </div>
       </div>
       {/* Bottom Nav Ends */}
